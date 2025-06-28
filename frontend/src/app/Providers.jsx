03a72b2f45bb576d30a6934d7432c1f2f4cb0b8d
salah almalawi <152004352+salah-almalawi@users.noreply.github.com>
2025-06-28 12:41:40 +0300

    Update Providers component

--- frontend/src/app/Providers.jsx
+++ frontend/src/app/Providers.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { Provider } from "react-redux";
-import store from "@/store";
+import store from "@/store/index";
 
 export default function Providers({ children }) {
   return <Provider store={store}>{children}</Provider>;
